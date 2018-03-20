e51404812758a5de5e5507aaa8f2c925e7e1c497
Baba <baba.sariffodeen.1@gmail.com>
2018-03-20 16:47:50 +0000

    added edit & save button functionality
    added edit & save button functionality

--- README.md
+++ README.md
@@ -3,3 +3,9 @@ DOM Scripting Course
 
 FORM SUBMIT EVENT
 -Forms have a submit event, which gets fired when user either clicks the submit button or the user simply hits Enter.
+
+CHANGE EVENT
+-The CHANGE event is fired when a change to an <input>, <select> or <textarea> value is made by the user.
+
+SUBMIT EVENT
+-This is triggered when user either clicks on submit OR presses Enter.
--- app.js
+++ app.js
@@ -3,17 +3,81 @@ const form = document.querySelector('#registrar');
 // const input = document.querySelector('#registrar > input'); better alternative below
     //We can use querySelctor on the form itself, as opposed to on the document. So we get closer to our target
 const input = form.querySelector('input');
-
+const ul = document.querySelector('#invitedList');
 
 form.addEventListener('submit', (event) => {
     event.preventDefault();
     const text = input.value;
-    input.value = "";
-    
-    const ul = document.querySelector('#invitedList');
+    input.value = '';
+
+    const li = createLI();
+
+    //append listItem to the parent ul
+    ul.appendChild(li);
+});
+
+function createLI(text){
+    //create listItem
     const li = document.createElement('li');
     li.textContent = text;
-    ul.appendChild(li);
 
+    //create label
+    const label = document.createElement('label');
+    label.textContent = 'Confirmed';
+
+    //create checkbox input
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+
+    //append checkbox input to label
+    label.appendChild(checkbox);
+    //append label to listItem
+    li.appendChild(label);
+
+    //create edit button
+    const editButton = document.createElement('button');
+    editButton.textContent = 'Edit';
+    li.appendChild(editButton);
+
+    //create remove button
+    const removeButton = document.createElement('button');
+    removeButton.textContent = 'Remove';
+    //append remove button to listItem
+    li.appendChild(removeButton); 
+
+    return li;
+}
+
+// Event handler on the ul, when any of the checkboxes are checked. Thanks to event bubbling.
+ul.addEventListener('change', (event) =>{
+    console.log(event.target.type);
+    const checkbox = event.target;
+    const checked = checkbox.checked;
+    const listItem = checkbox.parentNode.parentNode;
     
+    if(checked){
+        listItem.className = 'responded';
+    }else{
+        listItem.className = '';        
+    }
+});
+
+ul.addEventListener('click', (event) => {
+
+    if (event.target.tagName === 'BUTTON'){
+        const button = event.target;
+
+        if (button.textContent === 'Remove'){
+            const listItem = button.parentNode;
+            ul.removeChild(listItem);
+        }else{
+            if (button.textContent === 'Edit'){
+                button.textContent = 'Save';
+                
+            }else if (button.textContent === 'Save'){
+                button.textContent = 'Edit';
+                
+            }
+        }
+    }
 });
